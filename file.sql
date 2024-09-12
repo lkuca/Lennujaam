dc7a5adfa6ff2df48f585a81f0cd6721ebc0db3e
lkuca <120181604+lkuca@users.noreply.github.com>
2024-09-12 14:51:10 +0300

    tabeli loomine

---
+++ file.sql
@@ -0,0 +1,54 @@
+use lennujaam;
+
+create table lennujaamtable (
+	id INT,
+	lennujaam_nimi VARCHAR(50),
+	aeg VARCHAR(50),
+	summa VARCHAR(50),
+	lennujaam_asukoht VARCHAR(50),
+	lennujaam_kood VARCHAR(50),
+	lennujaam_riigi_kood VARCHAR(50),
+	lennujaam_GPS_kood VARCHAR(50)
+);
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (1, 'Iraan Municipal Airport', 'Asia/Chongqing', '$867.58', 'NA', 'IRB', 'US', null);
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (2, 'Vilhelmina Airport', 'Asia/Manila', '$481.80', 'EU', 'VHM', 'SE', 'ESNV');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (3, 'Pune Airport', 'America/Caracas', '$365.20', 'AS', 'PNQ', 'IN', 'VAPO');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (4, 'Bunsil Airport', 'Europe/Stockholm', '$926.38', 'OC', 'BXZ', 'PG', 'AYNS');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (5, 'Purdue University Airport', 'Asia/Tokyo', '$998.69', 'NA', 'LAF', 'US', 'KLAF');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (6, 'Chautauqua County-Dunkirk Airport', 'Asia/Makassar', '$934.39', 'NA', 'DKK', 'US', 'KDKK');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (7, 'Imroz Airport', 'America/Los_Angeles', '$800.97', 'AS', 'GKD', 'TR', 'LTFK');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (8, 'Lubango Airport', 'Africa/Porto-Novo', '$356.29', 'AF', 'SDD', 'AO', 'FNUB');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (9, 'McMinn County Airport', 'America/Guayaquil', '$544.95', 'NA', 'MMI', 'US', 'KMMI');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (10, 'Wanigela Airport', 'Europe/Tallinn', '$912.84', 'OC', 'AGL', 'PG', 'AYWG');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (11, 'Jimma Airport', 'Asia/Harbin', '$939.19', 'AF', 'JIM', 'ET', 'HAJM');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (12, 'Sliač Airport', 'Asia/Tehran', '$743.01', 'EU', 'SLD', 'SK', 'LZSL');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (13, 'Erbil International Airport', 'America/Asuncion', '$970.97', 'AS', 'EBL', 'IQ', 'ORER');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (14, 'Hongyuan Airport', 'Asia/Jakarta', '$643.13', 'AS', 'AHJ', 'CN', 'ZUHY');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (15, 'Lightning Ridge Airport', 'Asia/Makassar', '$432.71', 'OC', 'LHG', 'AU', 'YLRD');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (16, 'Bancasi Airport', 'Asia/Yekaterinburg', '$726.85', 'AS', 'BXU', 'PH', 'RPME');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (17, 'King Salman Abdulaziz Airport', 'Asia/Karachi', '$971.57', 'AS', 'DWD', 'SA', 'OEDM');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (18, 'Sabi Sabi Airport', 'Asia/Ho_Chi_Minh', '$626.15', 'AF', 'GSS', 'ZA', 'FASE');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (19, 'First Flight Airport', 'Europe/Warsaw', '$855.85', 'NA', 'FFA', 'US', 'KFFA');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (20, 'W K Kellogg Airport', 'Asia/Harbin', '$621.94', 'NA', 'BTL', 'US', 'KBTL');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (21, 'Rawlins Municipal Airport/Harvey Field', 'Asia/Makassar', '$824.77', 'NA', 'RWL', 'US', 'KRWL');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (22, 'El Nouzha Airport', 'Asia/Thimphu', '$598.34', 'AF', 'ALY', 'EG', 'HEAX');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (23, 'Dalnerechensk Airport', 'Europe/Volgograd', '$750.01', 'EU', 'DLR', 'RU', 'UHHD');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (24, 'Gostomel Airport', 'Asia/Jakarta', '$339.50', 'EU', 'GML', 'UA', 'UKKM');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (25, 'Alto Molocue Airport', 'Asia/Ulaanbaatar', '$949.73', 'AF', 'AME', 'MZ', null);
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (26, 'Okha Airport', 'America/Fortaleza', '$901.96', 'AS', 'OHH', 'RU', 'UHSH');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (27, 'Platov International Airport', 'Asia/Jakarta', '$872.88', 'EU', 'ROV', 'RU', 'URRP');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (28, 'Nalchik Airport', 'Asia/Shanghai', '$447.32', 'EU', 'NAL', 'RU', 'URMN');
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (29, 'Mary River Aerodrome', 'America/Havana', '$892.59', 'NA', 'YMV', 'CA', null);
+insert into lennujaamtable (id, lennujaam_nimi, aeg, summa, lennujaam_asukoht, lennujaam_kood, lennujaam_riigi_kood, lennujaam_GPS_kood) values (30, 'Kithira Airport', 'Asia/Chongqing', '$874.42', 'EU', 'KIT', 'GR', 'LGKC');
+
+
+select 
+
+lennujaam_nimi as '@lennujaam_nimi', 
+lennujaam_nimi,
+aeg,
+summa as 'reis/summa',
+lennujaam_asukoht,
+lennujaam_kood,
+lennujaam_riigi_kood,
+lennujaam_GPS_kood from lennujaamtable for xml path('lennujaamtable'), ROOT ('lennujaam');
